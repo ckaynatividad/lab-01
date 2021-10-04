bd505914fe2dbde2a03adbf338a1077b1a70bc70
ckaynatividad <natividad.ckay@gmail.com>
2021-10-04 16:27:31 -0700

    added title, inputs, random number, guesses remaining, play again button

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
-// import functions and grab DOM elements
 
-// initialize global state
+const button = document.getElementById('submit');
+var guessRemaining = document.getElementById('guessRemaining');
+let guess = 0;
+const message = document.getElementById('message');
+const refresh = document.getElementById('refresh');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+  
+const randomNum = Math.floor(Math.random() * 20 +1);
+console.log(randomNum);
+
+button.addEventListener('click', ()=>{
+  
+  const userGuess = document.getElementById('userGuess').value;
+
+  if(userGuess == randomNum){ 
+      message.textContent = 'You got it!';
+
+    } else if (userGuess > randomNum) { 
+      guess++;
+      message.textContent = 'Too high.';
+    } else if (userGuess < randomNum) {
+      guess++;
+     message.textContent = 'Too low.';
+    }
+    if(guess > 2) {
+    message.textContent = `Sorry, it was ${randomNum}.`;
+    const playagain = document.getElementById('playagain');  
+    playagain.style.display = 'block';
+  }
+guessRemaining.textContent = 3 - guess;
+  
+
+});
+
+refresh.addEventListener('click', ()=>{
+  window.location.reload();
+  refresh.classList.add('hide');
+  
+})
+
